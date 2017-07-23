f0e2861a9d85459c76cd07406b38a2090004459e
Henrik Steen <henrist@henrist.net>
2017-07-23 18:53:29 +0200

    Pull when building new image

--- build-push.sh
+++ build-push.sh
@@ -4,5 +4,5 @@ set -ex
 tag=$(date +%Y%m%d-%H%M)
 image=923402097046.dkr.ecr.eu-central-1.amazonaws.com/jenkins2/slave:$tag
 
-docker build -t $image .
+docker build --pull -t $image .
 docker push $image
