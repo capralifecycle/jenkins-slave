4468b73f50f64860cd600f92b91491548afd10d7
Henrik Steen <henrist@henrist.net>
2017-07-27 06:36:17 +0200

    Remove obsolete build script

--- build-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-tag=$(date +%Y%m%d-%H%M)
-image=923402097046.dkr.ecr.eu-central-1.amazonaws.com/jenkins2/slave-wrapper:$tag
-
-docker build --pull -t $image .
-docker push $image
