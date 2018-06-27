d5f2a0b3ffcadc9d25b5f1dbec60727d13f46d19
Henrik Steen <henrist@henrist.net>
2018-06-27 22:10:34 +0200

    Fix test script for local tests

--- test.sh
+++ test.sh
@@ -2,13 +2,17 @@
 # Build and test similar to in CI
 set -eux
 
-docker build -t jenkins-slave .
-docker run \
-  --rm \
-  -it \
-  -v "$PWD:/data" \
-  -w /data \
-  -u root \
-  --entrypoint= \
-  jenkins-slave \
-  sh -c './jenkins/test-image.sh'
+for model in modern classic; do
+  echo "Testing $model"
+
+  docker build -t jenkins-slave-$model -f ./$model/Dockerfile .
+  docker run \
+    --rm \
+    -it \
+    -v "$PWD:/data" \
+    -w /data \
+    -u root \
+    --entrypoint= \
+    jenkins-slave \
+    sh -c './jenkins/test-image.sh'
+done
