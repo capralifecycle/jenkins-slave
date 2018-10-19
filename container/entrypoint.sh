62f9e7333444343d39229dc4f9dec2671a6722fc
Henrik Steen <henrist@henrist.net>
2018-10-19 08:06:43 +0200

    Add specialized slave for java 11 builds
    This is just a temporary solution to get quick Java 11 support.
    See inline comment for details.
    
    The Docker setup is based on the other classic slave setup, but ported
    to Debian instead of Alpine.

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 # inspired by https://github.com/carlossg/jenkins-swarm-slave-docker/blob/master/jenkins-slave.sh
+set -eu
 
 # Add jenkins user to group owning Docker socket
 if [ -e /var/run/docker.sock ]; then
     gid=$(stat -c %g /var/run/docker.sock)
-    addgroup -g $gid docker 2>/dev/null || :
-    adduser jenkins docker || :
+
+    if [ -e /etc/alpine-release ]; then
+        addgroup -g $gid docker 2>/dev/null || :
+        adduser jenkins docker || :
+    else
+        groupadd -g $gid docker 2>/dev/null || :
+        usermod -aG docker jenkins || :
+    fi
 fi
 
 # if `docker run` first argument start with `-` the user is passing jenkins swarm launcher arguments
@@ -21,7 +28,7 @@ fi
 if [ $launcher -eq 1 ]; then
     jar=$(ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1)
 
-    gosu jenkins java $JAVA_OPTS -jar $jar -fsroot "/home/jenkins" "$@"
+    gosu jenkins java ${JAVA_OPTS:-} -jar $jar -fsroot "/home/jenkins" "$@"
 else
     gosu jenkins "$@"
 fi
