c2251d8784605550bd862287f1e7943130c059ab
Henrik Steen <henrist@henrist.net>
2019-01-18 21:01:12 +0100

    Prune unused networks

--- container/run-background-cleanup.sh
+++ container/run-background-cleanup.sh
@@ -6,11 +6,17 @@ while true; do
   date
   echo "Running cleanup script"
 
+  # We are not using `docker system prune` because we
+  # want to target a different filter for the image pruning.
+
   # Remove all stopped containers.
   docker container prune --force
 
   # Remove all unused volumes.
   docker volume prune --force
+
+  # Remove all unused networks.
+  docker network prune --force --filter until=1h
 
   # Remove images created for more than 3 days ago.
   # The time limit is an attempt to keep some images in
