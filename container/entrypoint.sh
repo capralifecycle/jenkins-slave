c3b659f7d2e0a7fd1427ccf672e210e0130ea7a0
Henrik Steen <henrist@henrist.net>
2020-03-09 00:38:09 +0100

    Move editorconfig and review some formatting

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -4,31 +4,31 @@ set -eu
 
 # Add jenkins user to group owning Docker socket
 if [ -e /var/run/docker.sock ]; then
-    gid=$(stat -c %g /var/run/docker.sock)
+  gid=$(stat -c %g /var/run/docker.sock)
 
-    if [ -e /etc/alpine-release ]; then
-        addgroup -g $gid docker 2>/dev/null || :
-        adduser jenkins docker || :
-    else
-        groupadd -g $gid docker 2>/dev/null || :
-        usermod -aG docker jenkins || :
-    fi
+  if [ -e /etc/alpine-release ]; then
+    addgroup -g $gid docker 2>/dev/null || :
+    adduser jenkins docker || :
+  else
+    groupadd -g $gid docker 2>/dev/null || :
+    usermod -aG docker jenkins || :
+  fi
 fi
 
 # if `docker run` first argument start with `-` the user is passing jenkins swarm launcher arguments
 if [ $# -lt 1 ]; then
-    launcher=1
+  launcher=1
 else
-    case "$1" in
-    -*) launcher=1 ;;
-    *) launcher=0 ;;
-    esac
+  case "$1" in
+  -*) launcher=1 ;;
+  *) launcher=0 ;;
+  esac
 fi
 
 if [ $launcher -eq 1 ]; then
-    jar=$(ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1)
+  jar=$(ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1)
 
-    su-exec jenkins java ${JAVA_OPTS:-} -jar $jar -fsroot "/home/jenkins" "$@"
+  su-exec jenkins java ${JAVA_OPTS:-} -jar $jar -fsroot "/home/jenkins" "$@"
 else
-    su-exec jenkins "$@"
+  su-exec jenkins "$@"
 fi
